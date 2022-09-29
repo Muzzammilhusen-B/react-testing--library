316283d9795bdd1610c02b99af20f3149b8cc860
Muzzammilhusen-B <muzammilhusen.b@applocum.org>
2022-09-29 11:02:51 +0530

    App.js rendering a component example for input element

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from "react";
+import "./App.css";
 
 function App() {
+  const [search, setSearch] = useState("");
+
+  const handleChange = (event) => {
+    setSearch(event.tagrget.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Search value={search} onChange={handleChange}>
+        Search:
+      </Search>
+      <p>Search for {search ? search : "..."}</p>
     </div>
   );
 }
-
+function Search({value, onChange, children}) {
+  return (
+    <div>
+      <label htmlFor="search">{children}</label>
+      <input type="text" id="search" onChange={onChange} value={value} />
+    </div>
+  );
+}
 export default App;
--- src/tests/first.test.js
+++ src/tests/first.test.js
@@ -1,11 +1,14 @@
 import React from "react";
-import First from "./first";
+import First from "./First";
 import {render, screen} from "@testing-library/react";
 
 describe("First test going to run", () => {
   it("Let's start test", () => {
+    // screen.debug();
+
     render(<First />);
 
+    // eslint-disable-next-line testing-library/no-debugging-utils
     screen.debug();
   });
 });
